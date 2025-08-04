43f93fe71d24a061b9ea1e39757d9e74ebe2733e
sina hozhabri <sinahzdev@gmail.com>
2025-08-04 19:36:35 +0330

    add accounts app with initial structure and configuration

--- src/accounts/apps.py
+++ src/accounts/apps.py
@@ -3,4 +3,6 @@ from django.apps import AppConfig
 
 class AuthConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'auth'
+    name = 'accounts'
+    def ready(self) -> None:
+        from . import signals
---
+++ src/accounts/signals.py
@@ -0,0 +1,28 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from mypermit import permit
+from asgiref.sync import async_to_sync
+from permit import RoleAssignmentCreate
+
+   
+@receiver(signal=post_save,sender=User)
+def sync_created_user_with_permit(sender,instance,created,**kwargs):
+    if created:
+        user_data = {
+            "key":f"{instance.id}",
+        }
+        if instance.first_name:
+            user_data['first_name'] = f"{instance.first_name}"
+        else:
+            user_data['first_name'] = f"{instance.username}"
+        if instance.last_name:
+            user_data['last_name'] = f"{instance.last_name}"
+        if instance.email:
+            user_data['email'] = f"{instance.email}"
+            
+        async_to_sync(permit.api.users.sync)(user_data)
+    
+        assingment =RoleAssignmentCreate(user=f"{instance.id}",role="viewer",tenant="default")
+        
+        async_to_sync(permit.api.users.assign_role)(assingment)                                                        
